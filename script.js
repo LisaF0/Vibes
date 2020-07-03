4de35af8f68ef3fdbe55f726037dbd037f857a50
Foret <lisaforet71@gmail.com>
2020-07-03 16:42:24 +0200

    js

--- script.js
+++ script.js
@@ -51,8 +51,11 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
+    // toggle entre add et remove la class active sur le btn qui controle le contenu
     this.classList.toggle("active");
+
     var accordionContent = this.nextElementSibling;
+    // toggle entre hide et show le contenu
     if (accordionContent.style.maxHeight) {
       accordionContent.style.maxHeight = null;
     } else {
@@ -63,21 +66,88 @@ for (i = 0; i < acc.length; i++) {
 
 
 /* MODAL */
+// 1
+// var modal = document.getElementsByClassName("myModal");
+// var btn = document.getElementsByClassName(["far", "fas"]);  
+// var span = document.getElementsByClassName("close")[0];
+// var j = 0 ;
+// for(var j = 0; i < modal.length; j++){
+//   btn.onclick = function(){
+//     modal.style.display = "block";
+//   }
+//   span.onclick = function(){
+//     modal.style.display = "none";
+//   }
+//   window.onclick = function(event){
+//     if(event.target == modal){
+//       modal.style.display = "none";
+//     }
+//   }
+//  }
 
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("btn");  
-var span = document.getElementsByClassName("close")[0];
-// var i;
-// for(var i = 0; i < modal.length; i++){
-  btn.onclick = function(){
-    modal.style.display = "block";
-  }
-  span.onclick = function(){
-    modal.style.display = "none";
-  }
-  window.onclick = function(event){
-    if(event.target == modal){
-      modal.style.display = "none";
-    }
-  }
-// }
+
+//2
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("btn");  
+// var span = document.getElementsByClassName("close")[0];
+// // var i;
+// // for(var i = 0; i < modal.length; i++){
+//   btn.onclick = function(){
+//     modal.style.display = "block";
+//   }
+//   span.onclick = function(){
+//     modal.style.display = "none";
+//   }
+//   window.onclick = function(event){
+//     if(event.target == modal){
+//       modal.style.display = "none";
+//     }
+//   }
+// // }
+
+
+
+ var modals = document.getElementsByClassName("myModal");
+ var minis = document.getElementsByClassName("modal-mini");
+ var modimgs = document.getElementsByClassName("modal-img");
+ var j = 0;
+  
+ for (j = 0; j < modals.length; j++)
+ {
+     console.log(j);
+     var mod = modals[j];
+     var imaj = modimgs[j];
+     minis[j].addEventListener("click", function()
+     {
+         // var mod = this.nextElementSibling;
+         mod.style.display = "block";
+         // var imaj = mod.img.children[0];
+         imaj.src = this.src;
+     })
+     var span = document.getElementsByClassName("close")[j];
+     span.addEventListener("click", function()
+     {
+         mod.style.display = "none";
+     })
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /* DARK MODE */
+
+//  function darkMode(){
+//    var element = document.main;
+//    element.classList.toggle("darkMode");
+//  }
