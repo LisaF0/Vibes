8c11198f228590b5d59506ff8cd516b2265c7c3e
LisaF0 <lisaforet71@gmail.com>
2020-07-06 16:58:31 +0200

    goutte ok, tri ok, accordion manque comment on fait
     pour en fermer un lorsqu'on ouvre un,
    boutton load more fonctionne mais bug,
    modal ne fonctionne toujours pas

--- script.js
+++ script.js
@@ -42,8 +42,6 @@ $(document).ready(function(){
 })
 
 // ACCORDION
-
-
 var acc = document.getElementsByClassName("accordionHeader");
 var i;
 
@@ -63,61 +61,20 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
-/* MODAL */
-
-
-
-//  var modals = document.getElementsByClassName("modal");
-//  var minis = document.getElementsByClassName("modal-mini");
-//  var modimgs = document.getElementsByClassName("modal-img");
-//  var j;
-  
-//  for (j = 0; j < modals.length; j++)
-//  {
-//      console.log(j);
-//      var mod = modals[j];
-//      var imaj = modimgs[j];
-//      minis[j].addEventListener("click", function()
-//      {
-//          // var mod = this.nextElementSibling;
-//          mod.style.display = "block";
-//          // var imaj = mod.img.children[0];
-//          imaj.src = this.src;
-//      })
-//      var span = document.getElementsByClassName("close")[j];
-//      span.addEventListener("click", function()
-//      {
-//          mod.style.display = "none";
-//      })
-//  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
  /* DARK MODE */
 
  document.addEventListener('DOMContentLoaded', () => {
 
   const themeStylesheet = document.getElementById('theme');
   const themeToggle = document.getElementById('theme-toggle');
+  // quand je clique sur l'élément avec l'id theme-toggle, ici la goutte : 
   themeToggle.addEventListener('click', () => {
-      // if it's light -> go dark
+      // si le stylesheet href inclu light alors le remplacer par 'dark-theme.css"
       if(themeStylesheet.href.includes('light')){
           themeStylesheet.href = 'dark-theme.css';
           
       } else {
-          // if it's dark -> go light
+          // si non remplacer par 'light-theme.css'
           themeStylesheet.href = 'light-theme.css';
           
 
@@ -126,15 +83,69 @@ for (i = 0; i < acc.length; i++) {
 })
 
 
+// TRI
+
+function hideAllExcept(filterName){ // hide all sauf filterName
+  if(filterName === "all"){ // si le filter c'est all
+    $("div.item") // alors toute les class item dans des div
+    .css('display','block')// on ajoute display block en css
+  } else {
+    $("div.item")// sinon pour toute les class item dans les div
+    .not("."+filterName) // qui ne sont pas = à .filterName on les hide
+    .hide()
+  }
+}
+
+$(function(){
+  $('.filter').click(function(){ // chaque fois qu'on clique sur un filtre
+    $('.item').hide().show()// ça hide ou show l'item
+
+    console.log(this.dataset.filter) // affiche les attr data des buttons
+    hideAllExcept(this.dataset.filter)//  hide tout sauf le filter concerné
+  })
+})
 
 
-//  function darkMode(){
-//    var element = document.main;
-//    element.classList.toggle("darkMode");
-//  }
+$(document).ready(function(){
+  $(".container").slice(0, 4).show(); // la classe container affiche 4 img
+  $("#loadMore").on("click", function(e){ // quand je clique sur le boutton loadmore, 
+    e.preventDefault(); //l'évènement par défaut
+    $(".container:hidden").slice(0, 4).slideDown();// les container hidden, slidedown par 4
+    if($(".container:hidden").length == 0) {// lorsqu'il n'y a plus de contenu en plus, 
+      $("#loadMore").text("No Content").addClass("noContent");// on modifie le boutton loadMore par noContent
+    }
+  });
+  
+})
 
-// function toggleDarkLight() {
-//   var body = document.getElementById("body");
-//   var currentClass = body.className;
-//   body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-// }
+
+
+/* MODAL */
+
+
+var modals = document.getElementsByClassName("modal");
+var minis = document.getElementsByClassName("container");
+var modimgs = document.getElementsByClassName("modal-img");
+var j;
+ 
+for (j = 0; j < modals.length; j++)
+{
+    console.log(j);
+    var mod = modals[j];
+    var imaj = modimgs[j];
+    var minii = minis[j];
+    minii.addEventListener("click", function()
+    {
+        // var mod = this.nextElementSibling;
+        mod.style.display = "block";
+        // var imaj = mod.img.children[0];
+        imaj.src = this.src;
+    })
+    var span = document.getElementsByClassName("close")[j];
+    span.addEventListener("click", function()
+    {
+        mod.style.display = "none";
+    })
+}
+
+
